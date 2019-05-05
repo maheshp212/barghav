d1666ff4ef883a012b6544804ed0f632b73872bd
Mahesh Perla <maheshp212@gmail.com>
2019-05-05 18:17:38 +0530

    sdfsd

--- app-filters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-appModule.filter('split', function() {
-        return function(input, splitChar, splitIndex) {
-            // do some bounds checking here to ensure it has that index
-            return input.split(splitChar)[splitIndex];
-        };
-    });
